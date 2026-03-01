3f4dc84a4ba560904db3505001f9e51ecf95479a
Mohammed Saqlain <saqlain14.ms@gmail.com>
2026-03-01 15:47:13 +0000

    reintialise

--- src/detection/test_detect.py
+++
@@ -1,8 +0,0 @@
-from detect_ui import UIDetector
-
-detector = UIDetector()
-detections = detector.detect(
-    r"C:\Workspcae\SelfHealing_UI_Automation\src\sample_ui.jpg")
-
-for d in detections:
-    print(d)
